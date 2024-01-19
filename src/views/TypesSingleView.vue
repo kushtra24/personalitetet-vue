32b3b232f54907ac971889d29b8365fd660215f2
kushtra24 <kushtra24@gmail.com>
2024-01-19 17:23:55 +0100

    types page and single types page added and finished

--- src/views/TypesSingleView.vue
+++ src/views/TypesSingleView.vue
@@ -1,7 +1,129 @@
 <script lang="ts" setup>
+import { ref, onMounted, onBeforeMount } from 'vue';
+import SidbarComponent from '@/components/SidbarComponent.vue';
+import { personalityTypesStore } from '@/stores/PersonalityTypesStore'
+import type { PersonalityType } from '@/types/PersonalityType';
+import axiosClient from '@/axiosClient';
+import { useRoute } from 'vue-router';
 
+
+const route = useRoute();
+const personalityTypeStore = personalityTypesStore();
+const personalityType = ref<PersonalityType>();
+const loading = ref<boolean>(false);
+onMounted(() => {
+  // if types don't exist in store fetch them form the backend
+  loading.value = true;
+  if(personalityTypeStore.typeSingleName) {
+    personalityTypeStore.fillSignleType(personalityTypeStore.typeSingleName);
+    personalityType.value = personalityTypeStore.typeSingle;
+    loading.value = false;
+  } else {
+    axiosClient.get(`api/type/${route.params.type}`)
+    .then((response) => {
+      personalityType.value = response.data;
+      if(personalityType.value) {
+        loading.value = false;
+      }
+    })
+    .catch((err) => {
+      console.log('Nope', err);
+      loading.value = false;
+    })
+  }
+});
+
+
+onBeforeMount(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+const stickyHeader = ref(false)
+function handleScroll(){
+  if (window.scrollY) {
+    stickyHeader.value = true
+  } else {
+    stickyHeader.value = false
+  }
+}
 </script>
 
 <template>
-<h1>types single page</h1>
+  <div class="container">
+
+    <img :src="`public/${personalityType?.type_img}`" :alt="`tipi-${personalityType?.name}`" width="100%" class="img-responsive">
+
+    <section id="type-single">
+      <font-awesome-icon v-if="loading" :icon="['fas', 'spinner']" spin  />
+      <div class="row" id="content" :class="{ 'displayflex': stickyHeader }" v-if="!loading">
+        <div class="col-md-9">
+          <h1>Tipi {{ personalityType?.type }}</h1>
+          <h4>Nryshe njihet edhe si {{ personalityType?.name }}</h4>
+          <hr>
+          <div class="description">
+            <div id="hyrja">
+              <div v-html="$t(personalityType?.type + 'intro')"></div>
+              <hr>
+            </div>
+            <div id="forcatDobesit">
+              <div v-html="$t( personalityType?.type + 'weaknessAndForces')"></div>
+              <hr>
+            </div>
+            <div id="lidhjet">
+               <div v-html="$t(personalityType?.type + 'relationships') "></div>
+              <hr>
+            </div>
+            <div id="miqesite">
+               <div v-html="$t(personalityType?.type + 'friends') "></div>
+              <hr>
+            </div>
+            <div id="siprinder">
+               <div v-html="$t(personalityType?.type + 'parents') "></div>
+              <hr>
+            </div>
+            <div id="profesioni">
+               <div v-html="$t(personalityType?.type + 'profession') "></div>
+              <hr>
+            </div>
+            <div id="vendipunes">
+               <div v-html="$t(personalityType?.type + 'workPlace') "></div>
+              <hr>
+            </div>
+            <div id="shtese">
+               <div v-html="$t(personalityType?.type + 'shortDescription') "></div>
+            </div>
+          </div>
+        </div>
+        <SidbarComponent :type="personalityType?.type" />
+      </div>
+
+    </section>
+  </div>
 </template>
+
+<style scoped>
+.description {
+  font-size: 18px;
+  text-align: justify;
+  line-height: 1.8;
+}
+
+.notLogedIn,
+.social-share,
+.placehodler {
+  background-color: #fff;
+  text-align: center;
+  padding: 15px;
+  margin: 15px 0;
+}
+
+.notLogedIn .btn,
+.social-share .btn,
+.placehodler .btn {
+  width: 100%;
+  margin: 10px 0;
+}
+
+.displayflex{
+display: flex;
+}
+</style>
--- src/views/TypesView.vue
+++ src/views/TypesView.vue
@@ -2,18 +2,21 @@
 import { ref, onMounted } from 'vue';
 import type { PersonalityType } from '../types/PersonalityType';
 import axiosClient from '@/axiosClient';
-import { personalityTypesStore } from '@/stores/personalityTypesStore'
+import { personalityTypesStore } from '@/stores/PersonalityTypesStore'
+import { useRouter } from 'vue-router';
+// import skeletonLoading from '@/components/SckeleltonLoading.vue';
+
 const personalityTypes = personalityTypesStore();
-
-
 const organizationTypes = ref<Array<PersonalityType>>();
 const selfTypes = ref<Array<PersonalityType>>();
 const friendsTypes = ref<Array<PersonalityType>>();
 const meetingTypes = ref<Array<PersonalityType>>();
+const loading = ref<boolean>(false);
 
 
 
 onMounted(() => {
+  loading.value = true;
   // if types don't exist in store fetch them form the backend
   if(!personalityTypes.types) {
     axiosClient.get('api/personalityTypes')
@@ -21,13 +24,16 @@ onMounted(() => {
       personalityTypes.types = response.data;
       if(personalityTypes.types?.length) {
         setTypes(personalityTypes.types);
+        loading.value = false;
       }
     })
     .catch((err) => {
       console.log('Nope', err);
+      loading.value = false;
     })
   } else if(personalityTypes.types?.length) {
     setTypes(personalityTypes.types);
+      loading.value = false;
     }
 });
 function splitArray(array: Array<PersonalityType>, chunkSize: number) {
@@ -46,6 +52,12 @@ function setTypes(types: Array<PersonalityType>) {
   meetingTypes.value = sections[3];
 }
 
+const router = useRouter();
+function forwardType(type: string) {
+  personalityTypes.typeSingleName = type;
+  router.push("type/" + type);
+}
+
 </script>
 
 <template>
@@ -62,48 +74,51 @@ function setTypes(types: Array<PersonalityType>) {
       <div id="organize" class="type-group row">
         <h1><strong>{{$t('organization')}}</strong></h1>
         <div class="col-md-3 the-type" v-for="personalityType in organizationTypes" :key="personalityType.id">
-          <router-link :to="{ path: 'type/' + personalityType.type, }">
+          <div @click="forwardType(personalityType.type)">
             <img :src="`public/${personalityType.type_img}`" :alt="`tipi-${personalityType.name}`">
             <h4>{{ personalityType.name }}</h4>
             <h5>{{ personalityType.type }}</h5>
-          </router-link>
+          </div>
         </div>
+        <font-awesome-icon v-if="loading" :icon="['fas', 'spinner']" spin  />
       </div>
 
       <div id="vetvetja" class="type-group row">
         <h1><strong>{{$t('self')}}</strong></h1>
         <div class="col-md-3 the-type" v-for="personalityType in selfTypes" :key="personalityType.id">
-          <router-link :to="{ path: 'type/' + personalityType.type, }">
+          <div @click="forwardType(personalityType.type)">
             <img :src="`public/${personalityType.type_img}`" :alt="`tipi-${personalityType.name}`">
             <h4>{{ personalityType.name }}</h4>
             <h5>{{ personalityType.type }}</h5>
-          </router-link>
+          </div>
         </div>
+        <font-awesome-icon v-if="loading" :icon="['fas', 'spinner']" spin  />
       </div>
 
       <div id="shoqeri" class="type-group row">
         <h1><strong>{{$t('friends')}}</strong></h1>
         <div class="col-md-3 the-type" v-for="personalityType in friendsTypes" :key="personalityType.id">
-          <router-link :to="{ path: 'type/' + personalityType.type, }">
+          <div @click="forwardType(personalityType.type)">
             <img :src="`public/${personalityType.type_img}`" :alt="`tipi-${personalityType.name}`">
             <h4>{{ personalityType.name }}</h4>
             <h5>{{ personalityType.type }}</h5>
-          </router-link>
+          </div>
         </div>
+        <font-awesome-icon v-if="loading" :icon="['fas', 'spinner']" spin  />
 
       </div>
 
       <div id="mbledhje" class="type-group row">
         <h1><strong>{{$t('meeting')}}</strong></h1>
         <div class="col-md-3 the-type" v-for="personalityType in meetingTypes" :key="personalityType.id">
-          <router-link :to="{ path: 'type/' + personalityType.type, }">
+          <div @click="forwardType(personalityType.type)">
             <img :src="`public/${personalityType.type_img}`" :alt="`tipi-${personalityType.name}`">
             <h4>{{ personalityType.name }}</h4>
             <h5>{{ personalityType.type }}</h5>
-          </router-link>
+          </div>
         </div>
-
+        <font-awesome-icon v-if="loading" :icon="['fas', 'spinner']" spin  />
       </div>
     </section>
   </div>
-</template>
+</template>@/stores/PersonalityTypesStore
